7100e66002c8faf338d226a7b3e26e7ebcf6a222
tushar69basak <tusharbasak2017@gmail.com>
2024-01-22 14:38:13 +0530

    make website responsive

--- js/script.js
+++ js/script.js
@@ -29,3 +29,103 @@ let accountForm = document.querySelector('.account-form');
 document.querySelector('#account-btn').onclick = () => {
   accountForm.classList.add('active');
 }
+
+document.querySelector('#close-form').onclick = () => {
+  accountForm.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  grabCursor: true,
+});
+
+var swiper = new Swiper(".home-courses-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+var swiper = new Swiper(".teachers-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".logo-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+let accordion = document.querySelectorAll(
+  '.faq .accordion-container .accordion'
+);
+
+accordion.forEach((acco) => {
+  acco.onclick = () => {
+    accordion.forEach((dion) => dion.classList.remove('active'));
+    acco.classList.toggle('active');
+  };
+})
+document.querySelector('.load-more .btn').onclick = () => {
+  document.querySelectorAll('.courses .box-container .hide').forEach((show) => {
+    show.style.display = 'block';
+  })
+  document.querySelector('.load-more .btn').style.display = 'none';
+}
